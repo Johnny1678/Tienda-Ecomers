c12ea7ea5847e0a80c410ee2fe905bd38eb77eb0
Johnny1678 <Johnny1678m@gmail.com>
2023-09-25 16:01:17 -0500

    Commit numero 1 de NestJs con Docker, y base de datos postgresSql

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('PLATZI STORE')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
+
+  await app.listen(3000, () => {
+    console.log(`Servidor escuchando por el puerto http://localhost:${3000}/`);
+  });
+}
+bootstrap();
